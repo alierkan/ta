d7a6562a55321d8e9f195d955ffe3bb126421efa
alierkan <Aysecik1>
2021-01-13 12:49:24 +0300

    forex

---
+++ forex/currency.py
@@ -0,0 +1,17 @@
+from forex_python.converter import CurrencyRates
+import datetime
+from datetime import timedelta, date
+
+
+def daterange(start_date, end_date):
+    for n in range(int((end_date - start_date).days)):
+        yield start_date + timedelta(n)
+
+
+c = CurrencyRates()
+start_date = date(2021, 1, 1)
+end_date = date(2021, 1, 13)
+for single_date in daterange(start_date, end_date):
+    date_obj = datetime.datetime(single_date.year, single_date.month, single_date.day, 18, 36, 28, 151012)
+    # print(single_date.strftime("%Y-%m-%d"))
+    print(c.get_rate('USD', 'TRY', date_obj))
